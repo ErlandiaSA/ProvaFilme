a2a44072e9dc9b7270e098b385be13de7e9a7a4c
ErlandiaSA <104632066+ErlandiaSA@users.noreply.github.com>
2022-07-31 21:25:30 -0300

    first commit

---
+++ provafilme/src/main/java/br/com/tech4me/provafilme/ProvafilmeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.provafilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvafilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvafilmeApplication.class, args);
+	}
+
+}
---
+++ provafilme/src/test/java/br/com/tech4me/provafilme/ProvafilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.provafilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvafilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
